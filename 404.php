470c942a8d9bb96811edfb19013cd4fb6e738ca9
Raukonim <Raukonim@users.noreply.github.com>
2014-11-10 15:22:40 +0100

    imatges 404 per tema

--- 404.php
+++ 404.php
@@ -6,44 +6,45 @@
 
 			<div class="type-page">
 			<?php
-						$random = rand(1,50);
-						if ($random <=10){
-							//Yuri Gagarin
-							echo '<h2 class="page-title">Error 404: He mirat i mirat, però no he vist a Déu</h2>';
+				// Generates a random 404 page
+				$random = rand(1,50);
+				if ($random <=10){
+					//Yuri Gagarin
+					echo '<h2 class="page-title">Error 404: He mirat i mirat, però no he vist a Déu</h2>';
 				
-							echo '<div class="entry">';
-								echo '<img src="http://www.maig.cat/wp-content/uploads/2014/11/gagarin.jpg" height="660" width="660">';
-							echo '</div>';
-						}elseif(($random <=20) && ($random >10)){
-							//Kruschev
-							echo "<h2 class='page-title'>Error 404: Aquesta pàgina no es troba disponible després de la desestanilintació d'aquesta revista</h2>";
+					echo '<div class="entry">';
+						echo '<div class="gagarin"></div>';
+					echo '</div>';
+				}elseif(($random <=20) && ($random >10)){
+					//Kruschev
+					echo "<h2 class='page-title'>Error 404: Aquesta pàgina no es troba disponible després de la desestanilintació d'aquesta revista</h2>";
 				
-							echo'<div class="entry">';
-								echo '<img src="http://www.maig.cat/wp-content/uploads/2014/11/kruschev.jpg" height="267" width="369">';
-							echo'</div>';
-						}elseif(($random <=30) && ($random >20)){
-							//El Che
-							echo '<h2 class="page-title">Error 404: Crear dos, tres... moltes editorials és la consigna!</h2>';
+					echo'<div class="entry">';
+						echo '<div class="kruschev"></div>';
+					echo'</div>';
+				}elseif(($random <=30) && ($random >20)){
+					//El Che
+					echo '<h2 class="page-title">Error 404: Crear dos, tres... moltes editorials és la consigna!</h2>';
 				
-							echo '<div class="entry">';
-								echo '<img src="http://www.maig.cat/wp-content/uploads/2014/11/elche.jpg" height="387" width="580">';
-							echo '</div>';
-						}elseif(($random <40) && ($random >=30)){
-							//Lenin
-							echo '<h2 class="page-title">Error 404: Acabem amb el contingut inexistent</h2>';
+					echo '<div class="entry">';
+						echo '<div class="elche"></div>';
+					echo '</div>';
+				}elseif(($random <40) && ($random >=30)){
+					//Lenin
+					echo '<h2 class="page-title">Error 404: Acabem amb el contingut inexistent</h2>';
 				
-							echo '<div class="entry">';
-								echo '<img src="http://www.maig.cat/wp-content/uploads/2014/11/lenin.png" height="382" width="500">';
-							echo '</div>';
-						}else{
-							//MIR
-							echo '<h2 class="page-title">Error 404: No hem pogut trobar la pàgina als nostres servidors espacials.</h2>';
+					echo '<div class="entry">';
+						echo '<div class="lenin"></div>';
+					echo '</div>';
+				}else{
+					//MIR
+					echo '<h2 class="page-title">Error 404: No hem pogut trobar la pàgina als nostres servidors orbitals.</h2>';
 				
-							echo '<div class="entry">';
-								echo '<img src="http://www.maig.cat/wp-content/uploads/2014/11/Mir.jpg" height="660" width="660">';
-							echo '</div>';
-						};
-					?>
+					echo '<div class="entry">';
+						echo '<div class="mir"></div>';
+					echo '</div>';
+				};
+			?>
 			</div>
 
 		</section>
